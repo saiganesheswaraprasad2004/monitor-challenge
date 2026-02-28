e59c1a79cb5df915bedc20a572d2191e55e0badd
Sai Ganesh Eswaraprasad <e.saiganesh@gmail.com>
2026-02-28 22:43:19 +0530

    Create mongodb.ts

---
+++ lib/mongodb.ts
@@ -0,0 +1,23 @@
+import { MongoClient } from "mongodb";
+
+const uri = process.env.MONGODB_URI as string;
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (!uri) {
+  throw new Error("Please add MONGODB_URI");
+}
+
+if (process.env.NODE_ENV === "development") {
+  if (!(global as any)._mongoClientPromise) {
+    client = new MongoClient(uri);
+    (global as any)._mongoClientPromise = client.connect();
+  }
+  clientPromise = (global as any)._mongoClientPromise;
+} else {
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;

ed4853ba023f3c7fa229b92959c86ef0b4294c5b
Sai Ganesh Eswaraprasad <e.saiganesh@gmail.com>
2026-02-28 22:49:20 +0530

    Create route.ts

---
+++ app/api/leaderboard/route.ts
@@ -0,0 +1,17 @@
+export const runtime = "nodejs";
+
+import { NextResponse } from "next/server";
+import clientPromise from "@/lib/mongodb";
+
+export async function GET() {
+  const client = await clientPromise;
+  const db = client.db("quizDB");
+
+  const users = await db
+    .collection("participants")
+    .find({ payment_stat: true })
+    .sort({ score: -1, time_taken: 1 })
+    .toArray();
+
+  return NextResponse.json(users);
+}

5b21df1d98bf6759b797d8760fe6b962885edc4c
Sai Ganesh Eswaraprasad <e.saiganesh@gmail.com>
2026-02-28 23:04:30 +0530

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 const uri = process.env.MONGODB_URI as string;
 
 if (!uri) {
-  throw new Error("Please define MONGODB_URI");
+  throw new Error("MONGODB_URI not defined");
 }
 
 let client: MongoClient;

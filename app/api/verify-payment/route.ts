993d65e75a923da755f97683ded0d8e2dda95bea
Sai Ganesh Eswaraprasad <e.saiganesh@gmail.com>
2026-02-28 20:33:56 +0530

    Update route.ts

--- app/api/verify-payment/route.ts
+++ app/api/verify-payment/route.ts
@@ -1,19 +1,47 @@
+export const runtime = "nodejs";
+
 import { NextResponse } from "next/server";
 import crypto from "crypto";
+import { createClient } from "@supabase/supabase-js";
 
 export async function POST(req: Request) {
   const body = await req.json();
 
-  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = body;
+  const {
+    razorpay_order_id,
+    razorpay_payment_id,
+    razorpay_signature,
+    name,
+    email,
+    phone,
+    genre,
+  } = body;
 
   const generated_signature = crypto
     .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET!)
     .update(razorpay_order_id + "|" + razorpay_payment_id)
     .digest("hex");
 
-  if (generated_signature === razorpay_signature) {
-    return NextResponse.json({ success: true });
-  } else {
+  if (generated_signature !== razorpay_signature) {
     return NextResponse.json({ success: false });
   }
+
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+
+  await supabase.from("participants").insert([
+    {
+      name,
+      email,
+      phone,
+      genre,
+      payment_stat: true,
+      score: 0,
+      time_taken: 0,
+    },
+  ]);
+
+  return NextResponse.json({ success: true });
 }

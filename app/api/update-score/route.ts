bebf93ae4e310aefb427edc26a5bb425c3ef530e
Sai Ganesh Eswaraprasad <e.saiganesh@gmail.com>
2026-02-28 23:05:00 +0530

    Update route.ts

--- app/api/update-score/route.ts
+++ app/api/update-score/route.ts
@@ -1,23 +1,17 @@
 export const runtime = "nodejs";
 
 import { NextResponse } from "next/server";
-import clientPromise from "@/lib/mongodb";
+import clientPromise from "../../../lib/mongodb";
 
 export async function POST(req: Request) {
-  const body = await req.json();
-  const { email, score, time_taken } = body;
+  const { email, score, time_taken } = await req.json();
 
   const client = await clientPromise;
   const db = client.db("quizDB");
 
   await db.collection("participants").updateOne(
     { email },
-    {
-      $set: {
-        score,
-        time_taken,
-      },
-    }
+    { $set: { score, time_taken } }
   );
 
   return NextResponse.json({ success: true });

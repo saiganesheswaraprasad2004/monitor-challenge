0f4b33d0e6f62af39503ec14c47445851de03b84
Sai Ganesh Eswaraprasad <e.saiganesh@gmail.com>
2026-02-28 23:37:46 +0530

    Update route.ts

--- app/api/leaderboard/route.ts
+++ app/api/leaderboard/route.ts
@@ -1,10 +1,12 @@
 export const runtime = "nodejs";
 
 import { NextResponse } from "next/server";
-import clientPromise from "../../../lib/mongodb";
+import { MongoClient } from "mongodb";
 
 export async function GET() {
-  const client = await clientPromise;
+  const client = new MongoClient(process.env.MONGODB_URI!);
+  await client.connect();
+
   const db = client.db("quizDB");
 
   const users = await db
@@ -12,6 +14,8 @@ export async function GET() {
     .find({ payment_stat: true })
     .sort({ score: -1, time_taken: 1 })
     .toArray();
+
+  await client.close();
 
   return NextResponse.json(users);
 }
